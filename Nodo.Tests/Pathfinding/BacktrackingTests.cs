99497e93b451b3531b8d3fc9fddec31a63a9c1f6
Jens Krumsieck <jenso666@live.de>
2022-08-28 20:43:20 +0200

    Added more stuff from chemsharp lib

---
+++ Nodo.Tests/Pathfinding/BacktrackingTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Nodo.Pathfinding;
+using Xunit;
+
+namespace Nodo.Tests.Pathfinding;
+
+public class BacktrackingTests
+{
+    [Fact]
+    public void Should_Produce_Valid_Paths()
+    {
+        var graph = new UndirectedGraph<int, Edge<int>>();
+        graph.Vertices.AddRange(new []{1,2,3,4,5,6,7,8,9,10});
+        graph.Edges.Add((1,2));
+        graph.Edges.Add((2,3));
+        graph.Edges.Add((2,4));
+        graph.Edges.Add((4,5));
+        graph.Edges.Add((5,6));
+        graph.Edges.Add((3,7));
+        graph.Edges.Add((7,8));
+        graph.Edges.Add((8,9));
+        graph.Edges.Add((9,10));
+
+        var start = 1;
+        var end = 10;
+        //path therefore should be 1,2,3,7,8,9,10 with a length of 7!
+        var result = Backtracking.BackTrack(start, end, graph.Neighbors, 7);
+        result.Should().HaveCount(7);
+        result.Should().BeEquivalentTo(new[] {1, 2, 3, 7, 8, 9, 10});
+    }
+}
---
+++ Nodo/Pathfinding/Backtracking.cs
@@ -0,0 +1,58 @@
+﻿namespace Nodo.Pathfinding;
+
+public static class Backtracking
+{
+    /// <summary>
+    ///     Uses backtracking for path detection of length limit
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="start"></param>
+    /// <param name="goal"></param>
+    /// <param name="neighbors"></param>
+    /// <param name="limit"></param>
+    /// <returns></returns>
+    public static HashSet<T> BackTrack<T>(T start, T goal, Func<T, IEnumerable<T>> neighbors, int limit)
+    {
+        var path = new HashSet<T> {start};
+        var maxSteps = 2000;
+        BackTrack(goal, path, neighbors, limit, ref maxSteps);
+        return path;
+    }
+    
+    /// <summary>
+    ///     Recursive Implementation of backtracking
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="goal"></param>
+    /// <param name="visited"></param>
+    /// <param name="neighbors"></param>
+    /// <param name="limit"></param>
+    /// <param name="maxSteps"></param>
+    /// <returns></returns>
+    private static bool BackTrack<T>(T goal, ISet<T> visited, Func<T, IEnumerable<T>> neighbors, int limit,
+        ref int maxSteps)
+    {
+        if (--maxSteps <= 0) return false;
+
+        var last = visited.Last();
+        if (Equals(last, goal) && visited.Count == limit)
+        {
+            visited.Add(goal);
+            return true;
+        }
+
+        if (visited.Count >= limit) return false;
+
+        foreach (var neighbor in neighbors(last))
+        {
+            if (visited.Contains(neighbor)) continue;
+
+            visited.Add(neighbor);
+            if (BackTrack(goal, visited, neighbors, limit, ref maxSteps)) return true;
+
+            visited.Remove(visited.Last());
+        }
+
+        return false;
+    }
+}
